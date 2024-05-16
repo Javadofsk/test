8b360c39a88d1aa5448333b8b08f00ab71dfd21a
Javadofsk <javadofsky@gmail.com>
2024-05-16 20:53:26 +0330

    Add files via upload

---
+++ MyContacts/Services/ContactsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace MyContacts
+{
+    class ContactsRepository : IContanctsRepository
+    {
+
+        private string connectionString = "Data Source=ARYA;Initial Catalog=Contact_DB;Integrated Security=true";
+        public bool Delete(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string name, string family, string mobile, string email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelcetRow(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            string query = "Select * From MyContacts";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public bool Update(int contactId, string name, string family, string mobile, string email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
